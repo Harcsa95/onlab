2e229f790f8c8fcb1a59e0d656d4298146cbe49f
Harcsa95 <harcsa.p.balint@gmail.com>
2018-03-20 09:57:33 +0100

    trying out the recipe api

---
+++ controller/RecipeList.controller.js
@@ -0,0 +1,28 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/demo/walkthrough/model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.walkthrough.controller.RecipeList", {
+		formatter : formatter,
+		onInit : function () {
+			this.getView().setModel(oViewModel, "view");
+		},
+		onFilterInvoices : function (oEvent) {
+
+			// build filter array
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+			}
+
+		
+		}
+
+	});
+});
