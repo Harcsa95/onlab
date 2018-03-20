cbc7c190e1b96d6abb56bac6aab2e0108dd0151a
Harcsa95 <harcsa.p.balint@gmail.com>
2018-03-20 12:44:24 +0100

    removing the unnecessary things

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -3,8 +3,6 @@ sap.ui.define([
 ], function (Controller) {
    "use strict";
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-    onOpenDialog : function () {
-        this.getOwnerComponent().openHelloDialog();
-    }
+    
    });
 });
